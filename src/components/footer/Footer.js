8db2812b16dd8a3b043f97014b24f3a4a77c7004
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2022-08-23 16:41:11 +1000

    update year in the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -41,7 +41,7 @@ export default function Footer() {
           </section>
           <section>
             <div className="footer-copyright">
-              2009-2019 © BosaNoga.ru — модный интернет-магазин обуви и
+              2009-2022 © BosaNoga.ru — модный интернет-магазин обуви и
               аксессуаров. Все права защищены.
               <br />
               Доставка по всей России!
